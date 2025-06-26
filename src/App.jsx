7a2915991e125569cccd4543bb4f611c4962c4a6
B-Mani-kandan <baskarmanikandan48@gmail.com>
2025-06-26 17:00:34 +0530

    Adding Contact Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Components/HomeContent/Home";
 import NavBar from "./Components/Header/NavBar";
 import Footer from "./Components/Footer/Footer";
+import ContactUs from "./Components/ContactUs/ContactUs";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/ContactUs" element={<ContactUs />} />
         </Routes>
         <Footer />
       </Router>
